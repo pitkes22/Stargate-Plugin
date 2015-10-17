ee749d995722c0a1030860fee23d78a37ccb93d2
pitkes22 <pitkes22.minecraft@gmail.com>
2015-10-17 23:25:39 +0200

    :/

--- src/me/pitkes22/stargate/Stargate.java
+++ src/me/pitkes22/stargate/Stargate.java
@@ -5,7 +5,7 @@ import org.bukkit.entity.ArmorStand;
 
 public class Stargate {
 
-	private Location OriginBlock;
+	private Location originBlock;
 	private ArmorStand stargateBase;
 	private ArmorStand stargateRing;
 	private ArmorStand chevron1Base; 
@@ -19,6 +19,33 @@ public class Stargate {
 	private ArmorStand chevron7; 
 	private ArmorStand chevron8; 
 	private ArmorStand chevron9;
+	
+	public Stargate(ArmorStand stargateBase, ArmorStand stargateRing, ArmorStand chevron1Base, ArmorStand chevron1Up,
+			ArmorStand chevron1Bottom, ArmorStand chevron2, ArmorStand chevron3, ArmorStand chevron4,
+			ArmorStand chevron5, ArmorStand chevron6, ArmorStand chevron7, ArmorStand chevron8, ArmorStand chevron9) {
+		// TODO Auto-generated constructor stub
+		
+		this.stargateBase = stargateBase;
+		this.stargateRing = stargateRing;
+		this.chevron1Base = chevron1Base;
+		this.chevron1Up = chevron1Up;
+		this.chevron1Bottom = chevron1Bottom;
+		this.chevron2 = chevron2;
+		this.chevron3 = chevron3;
+		this.chevron4 = chevron4;
+		this.chevron5 = chevron5;
+		this.chevron6 = chevron6;
+		this.chevron7 = chevron7;
+		this.chevron8 = chevron8;
+		this.chevron9 = chevron9;
+	
+	
+	}
+	
+	public Stargate(Location loc) {
+		String stargateBaseName = loc.getBlockX()+","+loc.getBlockY()+","+loc.getBlockZ()+","+"stargate_base";
+		//this.stargateBase = 
+	}
 	
 	public ArmorStand getStargateBase() {
 		return stargateBase;
@@ -126,27 +153,7 @@ public class Stargate {
 
 	
 	
-	public Stargate(ArmorStand stargateBase, ArmorStand stargateRing, ArmorStand chevron1Base, ArmorStand chevron1Up,
-			ArmorStand chevron1Bottom, ArmorStand chevron2, ArmorStand chevron3, ArmorStand chevron4,
-			ArmorStand chevron5, ArmorStand chevron6, ArmorStand chevron7, ArmorStand chevron8, ArmorStand chevron9) {
-		// TODO Auto-generated constructor stub
-		
-		this.stargateBase = stargateBase;
-		this.stargateRing = stargateRing;
-		this.chevron1Base = chevron1Base;
-		this.chevron1Up = chevron1Up;
-		this.chevron1Bottom = chevron1Bottom;
-		this.chevron2 = chevron2;
-		this.chevron3 = chevron3;
-		this.chevron4 = chevron4;
-		this.chevron5 = chevron5;
-		this.chevron6 = chevron6;
-		this.chevron7 = chevron7;
-		this.chevron8 = chevron8;
-		this.chevron9 = chevron9;
 	
-	
-	}
 	
  
 
